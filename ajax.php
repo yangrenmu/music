165b2d5947eae21352d7031330056a5d007ff21f
yangrenmu <1163393838@qq.com>
2017-03-07 14:17:46 +0800

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,5 +1,5 @@
 <?php
-	header("Access-Control-Allow-origin: *");
-	header("Access-Control-Allow-Methods: GET, POST");
+	header("Access-Control-Allow-origin": "*");
+	header("Access-Control-Allow-Methods": "GET, POST");
 	echo "hello world";
 ?>
