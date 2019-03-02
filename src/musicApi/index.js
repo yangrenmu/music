907fa7cad9036d6708373eb11744f1c5c419e6fc
yangrenmu <1163393838@qq.com>
2019-03-02 17:18:00 +0800

    change hot music list count

--- src/musicApi/index.js
+++ src/musicApi/index.js
@@ -1,7 +1,7 @@
 const _baseUrl = 'http://39.108.80.64:3000';
 export default {
   getPlayListByWhere (cat, order, offset, total, limit) {
-    return _baseUrl + '/top/playlist?limit=10&order=new';
+    return _baseUrl + '/top/playlist?limit=9&order=new';
   },
   getSong (id) {
     return _baseUrl + '/song/url?id=' + id;
